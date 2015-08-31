f4c200d1173fafba9a45307f90fcf3506020584b
Paul Sokolik <paul.sokolik@gmail.com>
2015-08-31 10:48:22 -0700

    remove filter/map function in end point and populate GeoJSON in forEach block

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ Lazy(fs.createReadStream('./listings.csv'))
   .forEach(function(line){
     var newLine = line.toString().split(',');
     var info = {
-      id: newLine[0],
+      id: Number(newLine[0]),
       street: newLine[1],
       status: newLine[2],
-      price: newLine[3],
-      bedrooms: newLine[4],
-      bathrooms: newLine[5],
-      sq_ft: newLine[6],
-      lat: newLine[7],
-      lng: newLine[8]
+      price: Number(newLine[3]),
+      bedrooms: Number(newLine[4]),
+      bathrooms: Number(newLine[5]),
+      sq_ft: Number(newLine[6]),
+      lat: Number(newLine[7]),
+      lng: Number(newLine[8])
     };
     if(info.bedrooms < minBed) {
       minBed = info.bedrooms;
@@ -46,22 +46,36 @@ Lazy(fs.createReadStream('./listings.csv'))
 
 
 app.get('/listings', function(req, res) {
-  console.log(req.query);
-  // var params = req.query;
+
+  var GeoJSON = {"type": "FeatureCollection", features: []};
   var min_price = req.query.min_price || minPrice;
   var max_price = req.query.max_price || maxPrice;
   var min_bed = req.query.min_bed || minBed;
   var max_bed = req.query.max_bed || maxBed;
   var min_bath = req.query.min_bath || minBath;
   var max_bath = req.query.max_bath || maxBath;
-  var results = allListings.filter(function(listing) {
-    // console.log(listing.price, listing.bathrooms, listing.bedrooms);
-    // console.log(min_price, max_price, min_bed, max_bed, min_bath, max_bath);
-    return (listing.price >= min_price && listing.price <= max_price)
-       &&  (listing.bathrooms >= min_bath && listing.bathrooms <= max_bath)
-       &&  (listing.bedrooms >= min_bed && listing.bedrooms <= max_bed);
+
+  allListings.forEach(function(listing) {
+    if ((listing.price >= min_price && listing.price <= max_price)
+    &&  (listing.bathrooms >= min_bath && listing.bathrooms <= max_bath)
+    &&  (listing.bedrooms >= min_bed && listing.bedrooms <= max_bed)) {
+      var listingInfo = {
+        type: "Feature",
+        geometry: {"type": "Point", "coordinates": [listing.lat, listing.lng]},
+        properties: {
+          "id": listing.id, 
+          "price": listing.price,
+          "street": listing.street,
+          "bedrooms": listing.bedrooms,
+          "bathrooms": listing.bathrooms,
+          "sq_ft": listing.sq_ft
+        }
+      }
+      GeoJSON.features.push(listingInfo);
+    }
   });
-  res.status(200).send(results);
+
+  res.status(200).send(GeoJSON);
 });
 
 app.listen(port);
